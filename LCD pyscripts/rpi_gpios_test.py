295fe5ee01a169306cb0106b457a2148b67e4288
Mohamed Gueni | محمد قاني <20975550+Gueni@users.noreply.github.com>
2025-05-13 21:18:39 +0100

    Refactor OLED display script and remove unused GPIO and network scripts

--- LCD pyscripts/rpi_gpios_test.py
+++ LCD pyscripts/rpi_gpios_test.py
@@ -1,16 +1,3 @@
-# from gpiozero import LED
-# from time import sleep
-
-# while True:
-#     GPIOS=[2,3,6,7,8,9,10,11,12,13,16,17,18,19,20,21]
-#     for gpio_index in GPIOS:
-#         print(" Testing GPIO :" +" "+ str(gpio_index))        
-#         led = LED(gpio_index)
-#         led.on()
-#         sleep(3)
-#         led.off()
-#         sleep(3)
-
 import RPi.GPIO as GPIO
 from time import sleep
 
